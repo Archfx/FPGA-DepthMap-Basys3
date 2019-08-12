ea34e074996ed9dc5da0e8e883da1bef8e5075b7
archfx <cybx@live.com>
2019-08-12 21:02:44 +0530

    VHDL implementation of Disparity image done

--- depthMap.srcs/sources_1/imports/disparity_generator.v
+++ depthMap.srcs/sources_1/imports/disparity_generator.v
@@ -21,7 +21,7 @@ module disparity_generator
 	input HCLK,										// clock
 	input [3:0] left_in,
 	input [3:0] right_in,	
-	output reg [3:0]  dOUT,				// Disparity out (even)
+	output [3:0]  dOUT,				// Disparity out (even)
     output [16:0]  dOUT_addr,				// Disparity out (even)	
     output [16:0]	left_right_addr,
 //    output [16:0]   right_addr,
@@ -37,11 +37,11 @@ reg 		ctrl_data_run;					// control signal for data processing
 reg [0 : (WIDTH*HEIGHT - 1)*4] org_L; 	// temporary storage for Left image
 reg [0 : (WIDTH*HEIGHT - 1)*4] org_R ;	// temporary storage for Right image
 
-reg [ 8:0] row; // row index of the image
+reg [8:0] row; // row index of the image
 reg [8:0] col; // column index of the Left image
 parameter window = 5;
-reg [3:0] x,y; // column index of the Right image
-reg [4:0] offset, best_offset;//, best_offset_1;
+integer x,y; // column index of the Right image
+reg [3:0] offset, best_offset;//, best_offset_1;
 localparam [4:0] maxoffset = 10; // Maximum extent where to look for the same pixel
 reg offsetfound;
 reg offsetping;
@@ -51,8 +51,10 @@ reg [20:0] prev_ssd;//, prev_ssd_1;
 reg [16:0] data_count; // data counting for entire pixels of the image
 reg [16:0] readreg;
 reg doneFetch;
+
 assign dOUT_addr= data_count;
 assign left_right_addr=readreg;
+assign dOUT=best_offset;
 
 always@(posedge HCLK) begin
     if (~doneFetch) begin
@@ -66,114 +68,213 @@ always@(posedge HCLK) begin
             doneFetch <=1;
         end
     end
-end
-
-
-always@(posedge HCLK,posedge doneFetch)
-begin
-if (doneFetch) begin
+    
+    //2
     if(ctrl_done) begin
-        row <= 0;
-		  col<= 0;
-		  offset<=4;
-		  offsetping<=0;
-		  compare<=0;
-		  ctrl_done <= 0;
-		  
+        data_count <= 0;
     end
-	else begin
-		
-		if(ctrl_data_run & offsetping==0 & compare==0) begin
-			if (offsetfound) begin
-				if(col == WIDTH - 1) begin
-					row <= row + 1;
-					
-				end
-				if(col == WIDTH - 1) begin
-					col <= 0;		
-				end
-				else begin 
-					col <= col + 1; // reading 2 pixels in parallel
-				end
-				offsetfound <= 0;
-				best_offset <= 0;
-				prev_ssd <= 65535;
-//				best_offset_1 <= 0;
-//				prev_ssd_1 <= 65535;
-			end
-			else begin
-				if(offset==maxoffset) begin
-					offsetfound <= 1;
-				end
-				else begin
-						offset<=offset+1;
-					//$display("row %d col %d  offset %d",row,col,offset);
-				end
-				ssd<=0;
-//				ssd_1<=0;
-				offsetping<=1;
-		end
-			
-		
-	end
-end
-end
-end
-always@(posedge offsetping) begin
-//	for(x=-(window-1)/2; x<((window-1)/2)+1; x=x+1) begin
-//			for(y=-(window-1)/2; y<((window-1)/2)+1; y=y+1) begin
+    else begin
+        if(ctrl_data_run)
+			data_count <= data_count + 1;
+    end
+    
+    ctrl_done <= (data_count == 308487)? 1'b1: 1'b0; // done flag308472
+    if (ctrl_done) doneFetch=0;
+    
+    //2
+    
+    //3
+    
+    if (offsetfound) begin
+        offset <= 4'd4;
+    end
+    //3
+    
+    //4
+    if (offsetping) begin
+        for(x=-(window-1)/2; x<((window-1)/2)+1; x=x+1) begin
+			for(y=-(window-1)/2; y<((window-1)/2)+1; y=y+1) begin
 				ssd=ssd+(org_L[((row + x ) * WIDTH + col + y)*4  +:4   ]-org_R[((row + x ) * WIDTH + col + y -offset)*4 +:4 ])*(org_L[((row +  x ) * WIDTH + col + y )*4 +:4   ]-org_R[((row +  x ) * WIDTH + col + y - offset)*4 +:4 ]);
 //				ssd_1=ssd_1+(org_L[(row + x ) * WIDTH + col + y  + 1 ]-org_R[(row + x ) * WIDTH + col + y -offset + 1 ])*(org_L[(row +  x ) * WIDTH + col + y  + 1 ]-org_R[(row +  x ) * WIDTH + col + y - offset + 1 ]);
+			end
+	   end
+	
+	   compare<=1;
+    end
+    
+    //4
+    
+    //5
+    if (compare) begin
+        if (ssd < prev_ssd ) begin
+            prev_ssd<=ssd;
+            best_offset<=offset;
+            
+        end
+	
+
+	
+	
+	   offsetping<=0;
+	   compare<=0;
+    end
+    //5
+    
+    //6
+    if (doneFetch) begin
+        if(ctrl_done) begin
+            row <= 0;
+              col<= 0;
+              offset<=4'd4;
+              offsetping<=0;
+              compare<=0;
+              ctrl_done <= 0;
+              
+        end
+        else begin
+            
+            if(ctrl_data_run & offsetping==0 & compare==0) begin
+                if (offsetfound) begin
+                    if(col == WIDTH - 1) begin
+                        row <= row + 1;
+                        
+                    end
+                    if(col == WIDTH - 1) begin
+                        col <= 0;		
+                    end
+                    else begin 
+                        col <= col + 1; // reading 2 pixels in parallel
+                    end
+                    offsetfound <= 0;
+                    best_offset <= 0;
+                    prev_ssd <= 21'd65535;
+    //				best_offset_1 <= 0;
+    //				prev_ssd_1 <= 65535;
+                end
+                else begin
+                    if(offset==maxoffset) begin
+                        offsetfound <= 1;
+                    end
+                    else begin
+                            offset<=offset+1;
+                        //$display("row %d col %d  offset %d",row,col,offset);
+                    end
+                    ssd<=0;
+    //				ssd_1<=0;
+                    offsetping<=1;
+            end
+                
+            
+        end
+end
+end
+    //6
+    
+end
+
+
+//always@(posedge HCLK,posedge doneFetch)
+//begin
+//if (doneFetch) begin
+//    if(ctrl_done) begin
+//        row <= 0;
+//		  col<= 0;
+//		  offset<=4'd4;
+//		  offsetping<=0;
+//		  compare<=0;
+//		  ctrl_done <= 0;
+		  
+//    end
+//	else begin
+		
+//		if(ctrl_data_run & offsetping==0 & compare==0) begin
+//			if (offsetfound) begin
+//				if(col == WIDTH - 1) begin
+//					row <= row + 1;
+					
+//				end
+//				if(col == WIDTH - 1) begin
+//					col <= 0;		
+//				end
+//				else begin 
+//					col <= col + 1; // reading 2 pixels in parallel
+//				end
+//				offsetfound <= 0;
+//				best_offset <= 0;
+//				prev_ssd <= 21'd65535;
+////				best_offset_1 <= 0;
+////				prev_ssd_1 <= 65535;
+//			end
+//			else begin
+//				if(offset==maxoffset) begin
+//					offsetfound <= 1;
+//				end
+//				else begin
+//						offset<=offset+1;
+//					//$display("row %d col %d  offset %d",row,col,offset);
+//				end
+//				ssd<=0;
+////				ssd_1<=0;
+//				offsetping<=1;
+//		end
+			
+		
+//	end
+//end
+//end
+//end
+//always@(posedge offsetping) begin
+//	for(x=-(window-1)/2; x<((window-1)/2)+1; x=x+1) begin
+//			for(y=-(window-1)/2; y<((window-1)/2)+1; y=y+1) begin
+//				ssd=ssd+(org_L[((row + x ) * WIDTH + col + y)*4  +:4   ]-org_R[((row + x ) * WIDTH + col + y -offset)*4 +:4 ])*(org_L[((row +  x ) * WIDTH + col + y )*4 +:4   ]-org_R[((row +  x ) * WIDTH + col + y - offset)*4 +:4 ]);
+////				ssd_1=ssd_1+(org_L[(row + x ) * WIDTH + col + y  + 1 ]-org_R[(row + x ) * WIDTH + col + y -offset + 1 ])*(org_L[(row +  x ) * WIDTH + col + y  + 1 ]-org_R[(row +  x ) * WIDTH + col + y - offset + 1 ]);
 //			end
 //	end
 	
-	compare<=1;
-end
+//	compare<=1;
+//end
 
-always @(posedge compare) begin	
+//always @(posedge compare) begin	
 
-	if (ssd < prev_ssd ) begin
-		prev_ssd<=ssd;
-		best_offset<=offset;
+//	if (ssd < prev_ssd ) begin
+//		prev_ssd<=ssd;
+//		best_offset<=offset;
 		
-	end
+//	end
 	
-//////	if (ssd_1 < prev_ssd_1 ) begin
-//////		prev_ssd_1<=ssd_1;
-//////		best_offset_1<=offset;
-//////	end
+
 	
 	
-	offsetping<=0;
-	compare<=0;
+//	offsetping<=0;
+//	compare<=0;
 	
-end
+//end
 
 //always@(posedge offsetfound) begin
-//	dOUT<=best_offset[3:0];
-////	DATA_1_L =best_offset_1*(255/maxoffset);
-//	//DATA_0_L=(org_L[WIDTH * row + col  ]+org_R[WIDTH * row + col  ])/2 ;
-//	//DATA_1_L =(org_L[WIDTH * row + col+1  ]+org_R[WIDTH * row + col+1  ])/2;
-//	offset <= 4;
+////	dOUT<=best_offset;
+//////	DATA_1_L =best_offset_1*(255/maxoffset);
+////	//DATA_0_L=(org_L[WIDTH * row + col  ]+org_R[WIDTH * row + col  ])/2 ;
+////	//DATA_1_L =(org_L[WIDTH * row + col+1  ]+org_R[WIDTH * row + col+1  ])/2;
+//	offset <= 4'd4;
 //end
 
 
 //////-------------------------------------------------//
 //////----------------Data counting---------- ---------//
 //////-------------------------------------------------//
-////always@(posedge HCLK)
-////begin
-////    if(ctrl_done) begin
-////        data_count <= 0;
-////    end
-////    else begin
-////        if(ctrl_data_run)
-////			data_count <= data_count + 1;
-////    end
+//always@(posedge HCLK)
+//begin
+//    if(ctrl_done) begin
+//        data_count <= 0;
+//    end
+//    else begin
+//        if(ctrl_data_run)
+//			data_count <= data_count + 1;
+//    end
     
-////    ctrl_done <= (data_count == 308487)? 1'b1: 1'b0; // done flag308472
-////    if (ctrl_done) doneFetch=0;
-////end
+//    ctrl_done <= (data_count == 308487)? 1'b1: 1'b0; // done flag308472
+//    if (ctrl_done) doneFetch=0;
+//end
 ////assign VSYNC = ctrl_vsync_run;
 
 ////-------------------------------------------------//
---
+++ depthMap.srcs/sources_1/imports/disparity_generator.vhd
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08/12/2019 04:50:30 PM
+-- Design Name: 
+-- Module Name: disparity_generator - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity disparity_generator is
+generic (window:positive:=5;
+         WIDTH:positive:=320;
+         HEIGHT:positive:=240;
+         maxoffset:positive:=10); --Maximum extent where to look for the same pixel
+  Port (
+    HCLK         : in  STD_LOGIC;
+	left_in      : in  STD_LOGIC_vector(3 downto 0);
+	right_in     : in  STD_LOGIC_vector(3 downto 0);	
+	dOUT         : out  STD_LOGIC_vector(3 downto 0);
+    dOUT_addr    : out  STD_LOGIC_vector(16 downto 0);
+    left_right_addr: out  STD_LOGIC_vector(16 downto 0);
+    ctrl_done    : out  STD_LOGIC;
+    offsetfound  : out  STD_LOGIC
+    		 );
+end disparity_generator;
+
+architecture Behavioral of disparity_generator is
+
+signal ctrl_data_run : std_logic;		--control signal for data processing
+
+type CacheArray is array(0 to WIDTH*HEIGHT-1) of std_logic_vector(3 downto 0);
+signal org_L : CacheArray; --temporary storage for Left image
+signal org_R : CacheArray; --temporary storage for Right image
+
+signal row :std_logic_vector(8 downto 0); --row index of the image
+signal col :std_logic_vector(8 downto 0); --column index of the Left image
+
+signal offset,best_offset :std_logic_vector(3 downto 0);
+signal offsetping,compare  : std_logic;
+
+signal ssd,prev_ssd :std_logic_vector(20 downto 0); --sum of squared difference
+
+signal data_count,readreg :std_logic_vector(16 downto 0); --data counting for entire pixels of the image
+signal doneFetch: std_logic;
+
+begin
+dOUT_addr<= data_count;
+left_right_addr<=readreg;
+dOUT<=best_offset;
+
+caching_process: process (HCLK) begin
+    if rising_edge(HCLK) then
+    if doneFetch='0' then
+        if unsigned(readreg)<76800 then
+             org_L(to_integer(unsigned(readreg)))<= left_in;
+             org_R(to_integer(unsigned(readreg)))<= right_in;
+--           org_L(unsigned(readreg) downto unsigned(readreg))<= left_in;
+--           org_R[readreg*4+:4]<= right_in;
+           readreg<=readreg+"1";
+--        end if;
+        else
+            readreg <= (others => '0');
+            doneFetch <='1';
+        end if;
+        
+    else
+            if unsigned(data_count)<7600 then
+                best_offset<=std_logic_vector(unsigned(org_L(to_integer(unsigned(data_count))))+unsigned(org_R(to_integer(unsigned(data_count))))/2);
+                data_count<=data_count+"1";
+            else
+                data_count <= (others => '0');
+                doneFetch <='0';
+            end if;  
+        end if;
+    end if;
+end process;
+
+end Behavioral;
