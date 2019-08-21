ba1c0477a36c24ca7d89f8d1a508186004816784
archfx <cybx@live.com>
2019-08-21 21:44:45 +0530

    small fixes

--- depthMap.srcs/sources_1/imports/vga.vhd
+++ depthMap.srcs/sources_1/imports/vga.vhd
@@ -8,36 +8,34 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 
 entity VGA is
-    Port ( CLK25 : in  STD_LOGIC;									-- Horloge d'entrée de 25 MHz							
-			  clkout : out  STD_LOGIC;					-- Horloge de sortie vers le ADV7123 et l'écran TFT
+    Port ( CLK25 : in  STD_LOGIC;									--  25 MHz							
+			  clkout : out  STD_LOGIC;					
            rez_160x120 : IN std_logic;
            rez_320x240 : IN std_logic;
-           Hsync,Vsync : out  STD_LOGIC;						-- les deux signaux de synchronisation pour l'écran VGA
-			  Nblank : out  STD_LOGIC;								-- signal de commande du convertisseur N/A ADV7123
+           Hsync,Vsync : out  STD_LOGIC;						
+			  Nblank : out  STD_LOGIC;								
            activeArea : out  STD_LOGIC;
-			  Nsync : out  STD_LOGIC);	-- signaux de synchronisation et commande de l'écran TFT
+			  Nsync : out  STD_LOGIC);	
 end VGA;
 
 architecture Behavioral of VGA is
-signal Hcnt:STD_LOGIC_VECTOR(9 downto 0):="0000000000";		-- pour le comptage des colonnes
-signal Vcnt:STD_LOGIC_VECTOR(9 downto 0):="1000001000";		-- pour le comptage des lignes
+signal Hcnt:STD_LOGIC_VECTOR(9 downto 0):="0000000000";		
+signal Vcnt:STD_LOGIC_VECTOR(9 downto 0):="1000001000";		
 signal video:STD_LOGIC;
-constant HM: integer :=799;	--la taille maximale considéré 800 (horizontal)
-constant HD: integer :=640;	--la taille de l'écran (horizontal)
-constant HF: integer :=16;		--front porch
-constant HB: integer :=48;		--back porch
-constant HR: integer :=96;		--sync time
-constant VM: integer :=524;	--la taille maximale considéré 525 (vertical)	
-constant VD: integer :=480;	--la taille de l'écran (vertical)
-constant VF: integer :=10;		--front porch
-constant VB: integer :=33;		--back porch
-constant VR: integer :=2;		--retrace
+constant HM: integer :=799;	
+constant HD: integer :=640;	
+constant HF: integer :=16;		
+constant HB: integer :=48;		
+constant HR: integer :=96;		
+constant VM: integer :=524;	
+constant VD: integer :=480;	
+constant VF: integer :=10;		
+constant VB: integer :=33;		
+constant VR: integer :=2;		
 
 begin
 
--- initialisation d'un compteur de 0 à 799 (800 pixel par ligne):
--- à chaque front d'horloge en incrémente le compteur de colonnes
--- c-a-d du 0 à 799.
+
 	process(CLK25)
 		begin
 			if (CLK25'event and CLK25='1') then
@@ -82,11 +80,11 @@ begin
 		end process;
 ----------------------------------------------------------------
 
--- génération du signal de synchronisation horizontale Hsync:
+-- Hsync:
 	process(CLK25)
 		begin
 			if (CLK25'event and CLK25='1') then
-				if (Hcnt >= (HD+HF) and Hcnt <= (HD+HF+HR-1)) then   --- Hcnt >= 656 and Hcnt <= 751
+				if (Hcnt >= (HD+HF) and Hcnt <= (HD+HF+HR-1)) then   
 					Hsync <= '0';
 				else
 					Hsync <= '1';
@@ -95,7 +93,7 @@ begin
 		end process;
 ----------------------------------------------------------------
 
--- génération du signal de synchronisation verticale Vsync:
+--  Vsync:
 	process(CLK25)
 		begin
 			if (CLK25'event and CLK25='1') then
@@ -108,9 +106,9 @@ begin
 		end process;
 ----------------------------------------------------------------
 
--- Nblank et Nsync pour commander le covertisseur ADV7123:
+
 Nsync <= '1';
-video <= '1' when (Hcnt < HD) and (Vcnt < VD)			-- c'est pour utiliser la résolution complète 640 x 480	
+video <= '1' when (Hcnt < HD) and (Vcnt < VD)			
 	      else '0';
 Nblank <= video;
 clkout <= CLK25;
